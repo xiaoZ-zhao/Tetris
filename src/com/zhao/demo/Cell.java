2f010afc8d842bc6ab10c22f84158eff67e4c287
xiaoZhao <zpzgzsby@163.com>
2022-05-11 15:07:06 +0800

    实现小方块类

---
+++ src/com/zhao/demo/Cell.java
@@ -0,0 +1,95 @@
+package com.zhao.demo;
+
+import java.awt.image.BufferedImage;
+import java.util.Objects;
+
+/**
+ * @author xiaoZhao
+ * @date 2022/5/7
+ * @describe
+ *  小方块类
+ *   方法： 左移、右移、下落
+ */
+public class Cell {
+
+    private int row;
+    private int col;
+    private BufferedImage image;
+
+    public Cell() {
+    }
+
+    public Cell(int row, int col, BufferedImage image) {
+        this.row = row;
+        this.col = col;
+        this.image = image;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Cell{" +
+                "row=" + row +
+                ", col=" + col +
+                ", image=" + image +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof Cell)) {
+            return false;
+        }
+        Cell cell = (Cell) o;
+        return getRow() == cell.getRow() &&
+                getCol() == cell.getCol() &&
+                Objects.equals(getImage(), cell.getImage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRow(), getCol(), getImage());
+    }
+
+    //左移动一格
+    private void left(){
+        col--;
+    }
+
+    //右移动一格
+    private void right(){
+        col++;
+    }
+
+    //下移动一格
+    private void down(){
+        row++;
+    }
+}
